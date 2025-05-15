e0936cb7b06879de838c58f18d9879039ef127ca
neikist <neikist1992@gmail.com>
2025-05-15 18:59:08 +0300

    fix continue config

--- home/username/.continue/install.sh
+++ home/username/.continue/install.sh
@@ -3,6 +3,8 @@
 CONFIG_DIR="$HOME/.continue"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cp "$SCRIPT_DIR/config.yaml" "$CONFIG_DIR/config.yaml"
-envsubst < "$CONFIG_DIR/config.yaml"
+if [[ -f "$CONFIG_DIR/config.yaml" ]]; then
+    rm -f "$CONFIG_DIR/config.yaml"
+fi
+envsubst < "$SCRIPT_DIR/config.yaml" >> "$CONFIG_DIR/config.yaml" 
 echo "Continue plugin config installed successful"
